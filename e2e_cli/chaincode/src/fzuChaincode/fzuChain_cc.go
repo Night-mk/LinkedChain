7b8f711ac956f74f935c653633b53dd9023234d6
zhang023 <1366534528@qq.com>
2018-07-10 11:23:01 +0800

    添加链码以及链码测试命令文档

--- e2e_cli/chaincode/src/fzuChaincode/fzuChain_cc.go
+++ e2e_cli/chaincode/src/fzuChaincode/fzuChain_cc.go
@@ -27,6 +27,7 @@ type applyIdentity struct {
 	corCheckIdentityId string `json:"cor_check_identity_id"`    //申请身份事件对应的审批身份事件的id
 }
 
+//申请基金事件结构
 type applyFund struct {
 	EventId string `json:"event_id"`        //事件id和类型
 	EventType string `json:"event_type"`
@@ -41,6 +42,7 @@ type applyFund struct {
 	corCheckFundId string `json:"cor_check_fund_id"`     //申请资金事件对应的审批资金事件的id
 }
 
+//审批身份事件结构
 type checkIdentity struct {
 	EventId string `json:"event_id"`               //事件id和类型
 	EventType string `json:"event_type"`
@@ -51,6 +53,7 @@ type checkIdentity struct {
 	CheckResult string `json:"check_result"`       //审批结果
 }
 
+//审批基金事件结构
 type checkFund struct {
 	EventId string `json:"event_id"`               //事件id和类型
 	EventType string `json:"event_type"`
@@ -61,18 +64,26 @@ type checkFund struct {
 	CheckResult string `json:"check_result"`       //审批结果
 }
 
+//初始化不做任何操作
 func (f *fzuChain) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	fmt.Println("init starts successfully ------------ !!!!")
 	return shim.Success(nil)
 }
 
+/*
+	读取函数名称和参数列表,在这里先不传参数
+ */
 func (f *fzuChain) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	funcName, args := stub.GetFunctionAndParameters()
+	fmt.Println()
 	fmt.Println("Invoke is running:  " + funcName + "  function ------!!!!!")
+	fmt.Println()
 
+	//判断执行什么操作
 	if funcName == "addEvent" {
 		return f.addEvent(stub, args)
-	} else if funcName == "getEvent" {
-		return f.getEvent(stub, args)
+	} else if funcName == "getEvents" {
+		return f.getEvents(stub, args)
 	} else if funcName == "deleteEvent" {
 		return f.deleteEvent(stub, args)
 	} else if funcName == "getEventsByRange" {
@@ -81,14 +92,16 @@ func (f *fzuChain) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 		return f.changeApplyStatus(stub, args)
 	}
 
-
 	fmt.Printf("%s invocation error", funcName)
 	return shim.Error("receive unknown function")
 }
 
+/*
+	添加事件
+ */
 func (f *fzuChain) addEvent(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	eventId := args[0]
-	eventType := args[1]
+	eventId := args[0]     //事件id
+	eventType := args[1]	//事件参数
 
 	//先判断事件存不存在
 	eventExists, err := stub.GetState(eventId)
@@ -120,7 +133,7 @@ func (f *fzuChain) addEvent(stub shim.ChaincodeStubInterface, args []string) pee
 		fmt.Println()
 
 		fmt.Printf("eventObject is -------%x------- ", eventJSONAsbytes)
-
+		fmt.Println()
 		err = stub.PutState(eventId, eventJSONAsbytes)
 		if err != nil {
 			return shim.Error(err.Error())
@@ -144,6 +157,7 @@ func (f *fzuChain) addEvent(stub shim.ChaincodeStubInterface, args []string) pee
 			return shim.Error(err.Error())
 		}
 
+		fmt.Println()
 		fmt.Println("eventId is -------" + eventId)
 		fmt.Println()
 
@@ -204,26 +218,49 @@ func (f *fzuChain) addEvent(stub shim.ChaincodeStubInterface, args []string) pee
 			return shim.Error(err.Error())
 		}
 	}
-
 	return shim.Success(nil)
 }
 
-func (f *fzuChain) getEvent(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	eventId := args[0]
+/*
+	获取单个事件，配合for循环构成getEvents函数
+ */
+func (f *fzuChain) getEvent(stub shim.ChaincodeStubInterface, args string) peer.Response {
+	eventId := args              //只传入一个参数
 
 	eventBytes, err := stub.GetState(eventId)
 	if err != nil {
 		return shim.Error(err.Error())
 	} else if eventBytes == nil {
-		return shim.Error("the user does not exist--------!!!")
+		return shim.Error("the event does not exist--------!!!")
 	}
 
-	fmt.Println("successfully get the user------!!!!")
-	fmt.Printf("the user message is -----%x------", eventBytes)
+	fmt.Println("successfully get the event " + eventId + "------!!!!")
+	fmt.Printf("the event message is -----%x------", eventBytes)
+	fmt.Println("-----------------")
+	fmt.Println()
 
 	return shim.Success(eventBytes)
 }
 
+/*
+	获取多个事件的信息，由用户输入想要查询的事件id
+	参数列表："Args":["getEvents", "a1", "b1"]
+ */
+func (f *fzuChain) getEvents(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	slice1 := args[0:]            //从参数开始读取，生成切片
+	slicelength := len(slice1)
+
+	for i := 0; i < slicelength; i++ {
+		f.getEvent(stub, slice1[i])
+	}
+
+	return shim.Success(nil)
+}
+
+
+/*
+	删除事件
+ */
 func (f *fzuChain) deleteEvent(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	eventId := args[0]
 
@@ -231,7 +268,7 @@ func (f *fzuChain) deleteEvent(stub shim.ChaincodeStubInterface, args []string) 
 	if err != nil {
 		return shim.Error(err.Error())
 	} else if eventBytes == nil {
-		return shim.Error("the user does not exist--------!!!")
+		return shim.Error("the event does not exist--------!!!")
 	}
 
 	err = stub.DelState(eventId)
@@ -239,10 +276,14 @@ func (f *fzuChain) deleteEvent(stub shim.ChaincodeStubInterface, args []string) 
 		return shim.Error(err.Error())
 	}
 
+	fmt.Println("successfully delete the event: " + eventId + "-------!!!!")
 	return shim.Success(nil)
 }
 
-//可以获取多个event的内容
+/*
+	可以获取多个event的内容（按顺序获取）
+	参数列表："Args":["getEventsByRange", "a1", "d1"],获取a1、b1、c1的内容
+ */
 func (f *fzuChain) getEventsByRange(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
@@ -293,6 +334,9 @@ func (f *fzuChain) getEventsByRange(stub shim.ChaincodeStubInterface, args []str
 	return shim.Success(buffer.Bytes())
 }
 
+/*
+	改变事件中某个字段的值
+*/
 func (f *fzuChain) changeApplyStatus(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	eventId := args[0]
 	eventType := args[1]
